b50754516c18b5fd14d4188f3c51a20ccb4341b5
Avan <mujibsardar@gmail.com>
2020-07-19 08:46:05 -0700

    BEFORE deploy

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+// app.use(express.static(path.join(__dirname, 'build')));
+const helmet = require('helmet') // creates headers that protect from attacks (security)
+const cors = require('cors')  // allows/disallows cross-site communication
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(helmet())
+app.use(cors(corsOptions))
+
+app.get('/api/', (req, res) => {
+  res.send({ people: 'You want to see people I assume' });
+});
+app.post('/api/', (req, res) => {
+  res.send(
+    `Person created: ${JSON.stringify(req.body.person)}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, (req, res) => {
+    console.log(`server listening on port: ${PORT}`)
+  });
